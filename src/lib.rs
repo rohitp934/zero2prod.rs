67936f39fc0df7e21c137935f65b983cf810b765
Rohit <rohitp934@gmail.com>
2023-04-29 11:28:26 +0530

    Use :0 port

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(addr: &str) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().service(health_check))
-        .bind("127.0.0.1:8000")?
+        .bind(addr)?
         .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -19,6 +19,6 @@ async fn health_check_test() {
 }
 
 fn spawn_app() {
-    let server = zero2prod::run().expect("failed to bind address");
+    let server = zero2prod::run("127.0.0.1:0").expect("failed to bind address");
     tokio::spawn(server);
 }
