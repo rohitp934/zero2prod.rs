890d5a1d430c46f465b6d1f81826cf4eb6a91569
Rohit <rohitp934@gmail.com>
2023-04-29 11:22:10 +0530

    Add first integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health_check))
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().service(health_check))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+    Ok(server)
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,24 @@
+//! tests/health_check.rs
+
+#[tokio::test]
+async fn health_check_test() {
+    // Arrange
+    spawn_app();
+
+    // Using reqwest to make HTTP requests to our application.
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://localhost:8000/health_check")
+        .send()
+        .await
+        .expect("failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("failed to bind address");
+    let _ = tokio::spawn(server);
+}
