159be539a432c7b74db325362d8026af2e4d5028
Rohit Prakash <rohitp934@gmail.com>
2023-05-07 20:58:48 +0530

    Added configuration and .env

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,39 @@
+//! src/configuration.rs
 
+#[derive(serde::Deserialize, Debug)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub port: u16
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub port: u16,
+    pub db_name: String
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!("postgres://{}:{}@{}:{}/{}",
+            self.username,
+            self.password,
+            self.host,
+            self.port,
+            self.db_name
+        )
+    }
+}
+
+
+pub fn get_config() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        // Adding configuration file as source
+        .add_source(config::File::with_name("configuration"))
+        .build()
+        .unwrap();
+
+    settings.try_deserialize()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 //! src/main.rs
 
-use zero2prod::startup::run;
+use zero2prod::{startup::run, configuration::get_config};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let config = get_config().expect("Unable to read from config file.");
+    println!("{:?}", config);
+    let address = format!("127.0.0.1:{}", config.port);
+    let listener = std::net::TcpListener::bind(&address).expect("failed to bind random port");
     println!(
         "Server listening on http://localhost:{}",
-        listener.local_addr().unwrap().port()
+        config.port
     );
     run(listener)?.await
 }
